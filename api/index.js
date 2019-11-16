4417b391489a7155b98bb442deb69e6886fb92fc
Chris Yunbin Chang <hw@yunbin.kr>
2019-11-17 06:44:44 +0900

    use compression for performance, delete outdated markers regulary

--- api/index.js
+++ api/index.js
@@ -8,9 +8,12 @@ aws.config.loadFromPath(__dirname + "/s3/config.json")
 var formidable = require('formidable')
 var fs = require('fs')
 var path = require('path')
+var compression = require('compression')
 var marker = require('./marker')
+var conn = require('./db')
 
 var app = express()
+app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
@@ -25,3 +28,15 @@ app.get('/', function(req, res) {
 app.listen(process.env.PORT || 3000, function() {
   console.log("listen on port 3000.")
 })
+
+setInterval(function() {
+  //Delete from markers WHERE marked_time < now() - INTERVAL 3 day;
+  const sql = 'DELETE FROM markers WHERE marked_time < now() - INTERVAL 3 DAY'
+  conn.query(sql, function(err, results) {
+    if (err) {
+      throw err
+    } else {
+      console.log(results.affectedRows + " deleted from the table.")
+    }
+  })
+}, 60 * 60 * 1000) // -> 1 hour.
